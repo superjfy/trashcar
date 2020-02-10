01ef6118a7847570148fc165863e5fe18fd37e9e
jeff.yang <jfyoung@ms10.hinet.net>
2020-02-10 14:10:00 +0800

    v1

---
+++ trashcar.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+
+req = requests.get("https://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json")
+data_dict = json.loads(req.text)
+city_len = len(data_dict)
+for i in range(city_len):
+    city_now = data_dict[i]["cityname"]
+    car_plat = data_dict[i]["car"]
+    data_time = data_dict[i]["time"]
+    if city_now == "新莊區":
+        print(data_dict[i]["location"], data_dict[i]["latitude"], data_dict[i]["longitude"])
+        if "銘德街" in data_dict[i]["location"]:
+            print(data_dict[i]["location"], data_dict[i]["latitude"], data_dict[i]["longitude"])
+
+
