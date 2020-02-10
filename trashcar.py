54c4b7d9e202eeeaf603029bf375dc268659a565
jeff.yang <jfyoung@ms10.hinet.net>
2020-02-10 14:54:51 +0800

    add lineNotify

--- trashcar.py
+++ trashcar.py
@@ -2,6 +2,20 @@
 import requests
 import json
 
+token = "URcWh4jqXmWSxmpiPoraXBDeTXu0WWcFO0fFMkIdyHp"
+
+
+def lineNotify(token, msg):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    payload = {'message': msg}
+    r = requests.post(url, headers=headers, params=payload)
+    return r.status_code
+
 
 req = requests.get("https://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json")
 data_dict = json.loads(req.text)
@@ -11,8 +25,8 @@ for i in range(city_len):
     car_plat = data_dict[i]["car"]
     data_time = data_dict[i]["time"]
     if city_now == u"新莊區":
-        print(data_dict[i]["location"], data_dict[i]["latitude"], data_dict[i]["longitude"])
         if u"銘德街" in data_dict[i]["location"]:
-            print(data_dict[i]["location"], data_dict[i]["latitude"], data_dict[i]["longitude"])
+            msg = data_dict[i]["location"]
+            lineNotify(token, msg)
 
 
